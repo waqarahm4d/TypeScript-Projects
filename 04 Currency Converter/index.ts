c599e821ebd239374870786fa445249674e8bdbd
Waqar Ahmad <cut3waqar@gmail.com>
2022-12-31 01:59:05 +0500

    Currency Converter Added

---
+++ 04 Currency Converter/index.ts
@@ -0,0 +1,52 @@
+import inquirer from "inquirer";
+
+let conversion = {
+    "PKR": {
+        "USD": 0.0044,
+        "GBP": 0.0037,
+        "PKR": 1
+    },
+    "USD": {
+        "USD": 1,
+        "GBP": 0.83,
+        "PKR": 225.50
+    },
+    "GBP": {
+        "USD": 1.21,
+        "GBP": 1,
+        "PKR": 271.75
+    }
+}
+
+const inputs: {
+    from: "PKR" | "USD" | "GBP",
+    to: "PKR" | "USD" | "GBP",
+    amount: number
+} = await inquirer.prompt([
+    {
+        type:"list",
+        name: "from",
+        choices:["PKR", "USD", "GBP"],
+        message:"Select the currency to convert from: "
+    },
+    {
+        type:"list",
+        name: "to",
+        choices:["PKR", "USD", "GBP"],
+        message:"Select currency to convert to: "
+    },
+    {
+        type:"number",
+        name: "amount",
+        message:"Enter Amount"
+    }
+])
+
+const {from, to, amount} = inputs
+if(from && to && amount){
+    let result = conversion[from][to] * amount
+    console.log(`The ${amount} in ${from} to ${to} equals to ${result}`)
+}else {
+    console.log("Try Again");
+    
+}
